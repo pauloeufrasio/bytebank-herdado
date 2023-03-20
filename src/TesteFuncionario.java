8a3f59b0ef9cae040bfbd0f1918021bc8b38870d
paulo <henriquepaulo0606@gmail.com>
2023-03-20 12:11:51 -0300

    2023-03-20-Criando classe funcionario

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,19 @@
+public class TesteFuncionario {
+
+    public static void main(String[] args) {
+
+        Funcionario funcionarioPaulo = new Funcionario();
+
+        funcionarioPaulo.setNome("Paulo");
+        funcionarioPaulo.setCpf("459.638.908-02");
+        funcionarioPaulo.setSalario(2600.00);
+
+        System.out.println(funcionarioPaulo.getNome());
+        System.out.println("Sua bonificação é de: " +funcionarioPaulo.getBonificacao());
+        System.out.println(funcionarioPaulo.getCpf());
+
+
+
+
+    }
+}
